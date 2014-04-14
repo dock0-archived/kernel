230be820d7f031e54fe0043b81914c5b74a4447c
Les Aker <me@lesaker.org>
2014-04-14 17:44:11 -0400

    fix gisting

--- autorun.sh
+++ autorun.sh
@@ -5,5 +5,6 @@ mkdir -p /opt/output
 rm -rf /opt/output/configs
 cp -pR /opt/configs /opt/output/configs
 cp /boot/vmlinuz* /opt/output/
+cd /opt/output/configs
 NEW_CONFIG="$(git status -s | awk '/??/ {print $2}')"
 gist -R /opt/kernels/configs/$NEW_CONFIG
