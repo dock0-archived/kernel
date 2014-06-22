fc6836da726ecf56a42aca1c17f3d6a4ee170e53
Les Aker <me@lesaker.org>
2014-06-22 10:58:22 -0400

    understand patches

--- autorun.sh
+++ autorun.sh
@@ -3,7 +3,7 @@
 [[ -n "$version" ]] && version="-k$version"
 
 git clone git://github.com/akerl/kernels /opt/configs
-roller.py -v $version -n next -b /opt/build -d /opt/configs || exit 1
+roller.py -v $version -n next -b /opt/build -d /opt/configs -p /opt/configs/patches/next || exit 1
 mkdir -p /opt/output
 rm -rf /opt/output/configs
 cp -pR /opt/configs /opt/output/configs
@@ -11,4 +11,5 @@ cp /boot/vmlinuz* /opt/output/
 cd /opt/output/configs
 git remote set-url origin git@github.com:akerl/kernels
 NEW_CONFIG="$(git status -s | awk '/??/ {print $2}')"
+cp -R /opt/output/configs/patches/next /opt/output/configs/patches/$NEW_CONFIG
 gist -R /opt/output/configs/$NEW_CONFIG
