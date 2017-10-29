78c531901c6c9db857f10cd49e570c29aa746220
Les Aker <me@lesaker.org>
2017-10-28 19:06:27 -0700

    move version parsing earlier up the chain

--- dev/version.py
+++ dev/version.py
@@ -22,6 +22,7 @@ def get_version(config):
             if 'Kernel Configuration' in line:
                 version = line.split(' ')[2]
                 break
+    version = version.replace('.0-rc', '-rc')
     return version
 
 
